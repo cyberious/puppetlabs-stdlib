728a7cce3101a45e6d0a4c3c095f09a0a5d8518d
Travis Fields <travis@puppetlabs.com>
2015-01-12 14:42:10 -0800

    Change from temp file to more controled directory location

--- lib/facter/facter_dot_d.rb
+++ lib/facter/facter_dot_d.rb
@@ -15,7 +15,7 @@
 class Facter::Util::DotD
     require 'yaml'
 
-    def initialize(dir="/etc/facts.d", cache_file="/tmp/facts_cache.yml")
+    def initialize(dir="/etc/facts.d", cache_file="#{File.join(Puppet['libdir'],'facts.cache')}")
         @dir = dir
         @cache_file = cache_file
         @cache = nil
